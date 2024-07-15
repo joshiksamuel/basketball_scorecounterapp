68fd4eacd25972fb61f188bd5275974b39ca10ad
joshiksamuel <joshiksamuel2003@gmail.com>
2024-07-15 15:20:32 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+let home_el=document.getElementById('home_p')
+let guest_el=document.getElementById('home_g')
+
+
+let count=0
+function home1(){
+    count +=1
+    home_el.textContent=count
+    
+}
+function home2(){
+    count +=2
+    home_el.textContent=count
+    
+}
+function home3(){
+    count +=3
+    home_el.textContent=count
+    
+}
+
+let count1=0
+function guest1(){
+    count1 +=1
+    guest_el.textContent=count1
+    
+}
+function guest2(){
+    count1 +=2
+    guest_el.textContent=count1
+    
+}
+function guest3(){
+    count1 +=3
+    guest_el.textContent=count1
+    
+}
